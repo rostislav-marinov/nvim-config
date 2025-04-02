c72221f53f9b5311a0da1488116477b98fe423dc
Rostislav Marinov <rostislav.marinov@gmail.com>
2025-04-02 20:05:21 +0300

    TreeSitter

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,4 +3,16 @@ return {
 
 { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -22,4 +22,4 @@ return { -- Highlight, edit, and navigate code
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  }
+}
